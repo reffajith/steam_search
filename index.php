c5fda01305f90a1267af0c9df07a57b6cffa16b7
Ajith Sebastian <reffajith@gmail.com>
2021-02-08 21:21:45 +0100

    Update index.php
    removed unwanted div

--- index.php
+++ index.php
@@ -14,7 +14,6 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      </div>
     </nav>
 
     <main role="main" class="container">
